ade9410b6c8a8a17d6e248d49f2aa658164c7112
rehan ilahi <rehanilahi680@gmail.com>
2026-06-18 21:43:48 +0500

    name change

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const DESSERT_OPTIONS = [
 const TOTAL_QUESTIONS = 5
 
 // The one this little experience is for 💕
-const LOVE_NAME = 'Ch Khadija'
+const LOVE_NAME = 'Atiqa'
 
 // framer-motion slide+fade variants for each step
 const variants = {
